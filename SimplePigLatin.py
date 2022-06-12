bb6b1e884f44195ee67369119dbe2281bbb8e075
mici-que <Maria.Kohler@ibm.com>
2022-06-12 16:03:11 +0200

    2a GREEN

--- SimplePigLatin.py
+++ SimplePigLatin.py
@@ -1,7 +1,26 @@
 def main(sentence=None):
+    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
     def validator(sentence=None):
         return "sentence" in vars() and isinstance(sentence, str)
 
     if validator(sentence):
-        return True
+        newString = ""
+        carrying = False
+        for character in sentence:
+            if character in letters:
+                if carrying == False:
+                    carry = character
+                    carrying = True
+                else:
+                    newString = newString + character
+            else:
+                if carrying == False:
+                    newString = newString + character
+                else:
+                    newString = newString + carry + "ay" + character
+                    carrying = False
+        if carrying:
+            newString = newString + carry + "ay"
+        return newString
     return False
